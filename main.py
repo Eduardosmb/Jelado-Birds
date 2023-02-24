7fc4277d8d540a8dab012e7efb7c0ffbfa37b2c2
Eduardosmb <edubarros253@gmail.com>
2023-02-23 21:25:43 -0300

    fase dificil

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ FPS = 60  # Frames per Second
 #variável pra contar a pontuação do jogador
 pontuacao = 0
 #variável pra contar as tentativas do jogador
-tentativas = 5
+tentativas = 100
 #obtendo o fonte usada na pontuação
 font = pygame.font.SysFont(None, 30)
 
@@ -190,6 +190,7 @@ if modo_facil == True:
                     inimigo_morto = True  
                     pontuacao+=1
                     tentativas = 5
+                    rodando = True
                     
 
 
@@ -213,3 +214,135 @@ if modo_facil == True:
 
     # Terminar tela
     pygame.quit()
+
+
+
+if modo_dificil == True:
+
+    barros_planeta = np.array([400, 400])
+    enzo_planeta = np.array([600, 300])
+
+    def gravidade(constante, planeta):
+        direcao_a = planeta - s # constante gravitacional * massa planeta
+        d = np.linalg.norm(direcao_a)
+        direcao_a = direcao_a / d
+        mag_a = constante / d**2
+        a = direcao_a * mag_a
+        return a
+    
+    while rodando:
+        # Capturar eventos
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                rodando = False
+
+        screen.blit(background, (100, 320), pygame.Rect(100, 320, 90, 90))
+        mouse_pos = pygame.mouse.get_pos()
+        # Calcular ângulo entre o canhão e o mouse
+        dx = mouse_pos[0] - canhao_rect.center[0]
+        dy = mouse_pos[1] - canhao_rect.center[1]
+        angle = np.degrees(np.arctan2(-dy, dx))
+        # Rotacionar imagem do canhão para o ângulo calculado
+        canhao_rot = pygame.transform.rotate(canhao, angle)
+        canhao_rect_rot = canhao_rot.get_rect(center=canhao_rect.center)
+        # Desenhar imagem rotacionada do canhão na tela
+        screen.blit(canhao_rot, (100, 320),canhao_rect_rot)
+        pygame.display.update()
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if tentativas < 2:
+                rodando = False
+            else:
+                tentativas -= 1
+
+            while True:
+                # Capturar eventos
+                if s[0]<5 or s[0]>pos[0] or s[1]<5 or s[1]>pos[1]: # Se eu chegar ao limite da tela, reinicio a posição do personagem
+                    v0 = (pygame.mouse.get_pos() - s0)
+                    v0 = v0 / np.linalg.norm(v0) * 100
+                    v0 = v0 + 3 * np.random.randn(2)
+                    s, v = s0, v0
+                    break
+
+                # Controlar frame rate
+                clock.tick(FPS)
+
+                # Desenhar fundo
+                screen.blit(background, (0, 0))
+                screen.blit(canhao_rot, (100, 320),canhao_rect_rot)
+                pygame.display.update()
+
+                # Processar posicoes
+                ac_barros = gravidade(40000, barros_planeta)
+                ac_enzo = gravidade(70000, enzo_planeta)
+                a = ac_barros + ac_enzo
+                v = v + a
+                s = s + 0.25 * v
+
+                # Desenhar personagem
+                rect = pygame.Rect(s, (10, 10))  # First tuple is position, second is size.
+                screen.blit(personagem, rect)
+                planet = pygame.draw.circle(screen, "red", barros_planeta, 20, 20)
+                planet2 = pygame.draw.circle(screen, "blue", enzo_planeta , 20, 20)
+
+                if inimigo_morto == True:
+                    #sorteia nova posição do inimigo
+                    posicao_inimigo = (random.randint(500, 1020), random.randint(100, 700))
+
+                    #sortea novo inimigo
+                    sorteador_inimigos = random.choice([ergio, guri])
+
+                    #declara estado do inimigo como vivo
+                    inimigo_morto = False
+
+                if inimigo_morto == False:
+                    # screen.blit(inimigo, posicao_inimigo)
+                    inimigo = pygame.draw.circle(screen, "green", posicao_inimigo, 20, 20)
+
+                    #feito
+                    if sorteador_inimigos == ergio:
+                        screen.blit(ergio, (posicao_inimigo[0]-29, posicao_inimigo[1]-31))
+
+                    #Feito
+                    if sorteador_inimigos == guri:
+                        screen.blit(guri, (posicao_inimigo[0]-33, posicao_inimigo[1]-45))
+
+                    if sorteador_inimigos == lucca:
+                        screen.blit(lucca, (posicao_inimigo[0]-29, posicao_inimigo[1]-31))
+                    if sorteador_inimigos == magno:
+                        screen.blit(magno, (posicao_inimigo[0]-29, posicao_inimigo[1]-31))
+                    if sorteador_inimigos == vaz:
+                        screen.blit(vaz, (posicao_inimigo[0]-29, posicao_inimigo[1]-31))
+                    if sorteador_inimigos == wever:
+                        screen.blit(wever, (posicao_inimigo[0]-29, posicao_inimigo[1]-31))
+                    if sorteador_inimigos == felipe_47:
+                        screen.blit(felipe_47, (posicao_inimigo[0]-29, posicao_inimigo[1]-31))
+                    if sorteador_inimigos == celao:
+                        screen.blit(celao, (posicao_inimigo[0]-29, posicao_inimigo[1]-31))
+                    if sorteador_inimigos == alfredo:
+                        screen.blit(alfredo, (posicao_inimigo[0]-29, posicao_inimigo[1]-31))
+                    
+
+                
+                if inimigo.collidepoint(s):
+                    inimigo_morto = True  
+                    pontuacao+=1
+                    tentativas = 5
+                    
+
+
+                #blitando a pontuação na tela
+                text = font.render('Pontos:' + str(pontuacao), False, (255,255,255))
+                textRect = text.get_rect()
+                textRect.center = (100, 100)
+                screen.blit(text, textRect)
+                text_tentativas = font.render('Tentativas:' + str(tentativas), False, (255,255,255))
+                textRect_tentativas = text_tentativas.get_rect()
+                textRect_tentativas.center = (100, 150)
+                screen.blit(text_tentativas, textRect_tentativas)
+
+
+                screen.blit(barros, ([pos[0]-448, pos[1]-490]))
+                screen.blit(enzo, ([pos[0]-650, pos[1]-390]))
+                # Update!
+                pygame.display.update()
